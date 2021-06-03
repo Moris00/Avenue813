efac524e8869223d28cb66d4728734489dec21e4
moris <m.ricco12@studenti.unisa.it>
2021-06-03 21:38:28 +0200

    dasdasd

---
+++ Avenue813/src/it/avenue813/utils/MainContext.java
@@ -0,0 +1,60 @@
+package it.avenue813.utils;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.sql.DataSource;
+
+public class MainContext implements ServletContextListener {
+	
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		Utility.print("Startup web application");
+		
+		ServletContext context = sce.getServletContext(); //Serve per salvare il nostro database
+		
+		DataSource ds = null;
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			
+			ds = (DataSource) envCtx.lookup("jdbc/database");
+				try {
+					Connection con = null;
+					try {
+						con = ds.getConnection();
+				
+						DatabaseMetaData metaData = con.getMetaData();
+						Utility.print("DBMS name: "+ metaData.getDatabaseProductName());
+						Utility.print("DBMS name: "+ metaData.getDatabaseProductVersion());
+				
+					}finally {
+						if(con != null) con.close();
+					}
+			
+				}catch(SQLException e) {
+					Utility.print(e);
+				}
+			}catch(NamingException e) {
+				Utility.print(e);
+			}
+		
+		context.setAttribute("DataSource", ds); //Database visto da tutte le servlet
+		Utility.print("DataSource creation: "+ds.toString());
+	}
+	
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		ServletContext context = sce.getServletContext();
+		context.removeAttribute("DataSource");
+		Utility.print("Shutdown web application");
+	}
+}
