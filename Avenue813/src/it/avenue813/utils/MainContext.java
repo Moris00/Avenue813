ac4be5fb8f291086e0bff64a01460c1e2b49795b
moris <m.ricco12@studenti.unisa.it>
2021-06-03 21:49:44 +0200

    sdasdas

--- Avenue813/src/it/avenue813/utils/MainContext.java
+++ Avenue813/src/it/avenue813/utils/MainContext.java
@@ -10,8 +10,10 @@ import javax.naming.NamingException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 import javax.sql.DataSource;
 
+@WebListener
 public class MainContext implements ServletContextListener {
 	
 	@Override
