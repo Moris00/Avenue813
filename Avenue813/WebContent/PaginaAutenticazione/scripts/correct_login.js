73b640c6f2644b6231d1b750b499858aeee0ba49
Moris00 <m.ricco12@studenti.unisa.it>
2021-06-12 14:34:10 +0200

    aadsa

--- Avenue813/WebContent/PaginaAutenticazione/scripts/correct_login.js
+++ Avenue813/WebContent/PaginaAutenticazione/scripts/correct_login.js
@@ -1,12 +1,3 @@
-function isUsername(obj){
-	var letters = /^[a-zA-Z0-9]+$/;
-		if(obj.value.match(letters)){
-			return true;
-		}else{
-			return false;
-		}
-}
-
-function isValidLogin(){
-
-}
+/**
+ * 
+ */
